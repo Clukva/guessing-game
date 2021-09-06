b23c1c1215ace85142bb75d1a4b2d606d0bd2802
Clukva <yura4-1-1@mail.ru>
2021-09-06 08:59:46 +0300

    feat: solution guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.compare = [];
+        this.middle;
+    }
 
     setRange(min, max) {
+        this.compare =[min, max];
 
     }
 
     guess() {
-
+        this.middle =Math.ceil((this.compare[1]-this.compare[0])
+        /2 + this.compare[0]);
+        return this.middle;
     }
 
     lower() {
+        this.compare[1] = this.middle;
+       
 
     }
 
     greater() {
-
+        this.compare[0] = this.middle;
+        
     }
 }
 
